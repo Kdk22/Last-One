bf017618ab19c00c3990d737f2b507115f90bf3d
DESKTOP-OG2K35M\KhadkaUtshab <KhadkaUtshab@DESKTOP-OG2K35M>
2017-02-07 20:45:33 +0545

    Third Commit

---
+++ jquery2.js
@@ -0,0 +1,33 @@
+
+
+function openNav() {
+    document.getElementById("accordion").style.width = "440px";
+   //document.getElementById("jssor_1").style.marginLeft= "450px";
+   
+ 
+}
+
+function closeNav() {
+    document.getElementById("accordion").style.width = "0px";
+    document.getElementById("jssor_1").style.marginLeft= "0px";
+   // document.getElementById("accordion").style.overflow-y="hidden";
+    
+
+}
+
+function about(){
+   
+           
+  /*  
+    var div = document.createElement('div');
+
+    div.className = 'row';
+
+    div.innerHTML = '<object type="type/html" data="about.html" ></object>';
+
+     document.getElementById('mySidenav').appendChild(div);
+    
+    
+document.getElementById('mySidenav #contact').innerHTML= '<object type="type/html" data="about.html" ></object>';
+*/
+}
